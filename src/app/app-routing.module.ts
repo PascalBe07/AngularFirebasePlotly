6d33017ed843effb0833b346b96a1f85e46d788e
PascalBe07 <pascal.betting@gmx.de>
2018-02-28 13:43:37 +0100

    feat(charts): add xy chart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {FormsModule} from "@angular/forms";
 import {environment} from "../environments/environment";
-import {MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule, MatToolbarModule} from "@angular/material";
+import {
+  MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule, MatTabsModule,
+  MatToolbarModule
+} from "@angular/material";
 import {AngularFireModule} from "angularfire2";
 import {AngularFireAuthModule} from "angularfire2/auth";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import { ChartsComponent } from './charts/charts.component';
 import {LoginGuard} from "./login.guard";
+import {AngularFireDatabaseModule} from "angularfire2/database";
 
 const routes: Routes = [
   { path: "login", component: LoginComponent },
@@ -21,8 +25,8 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes), FormsModule, BrowserAnimationsModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFireAuthModule,
-    MatInputModule, MatFormFieldModule, MatButtonModule, MatIconModule
+    AngularFireAuthModule, AngularFireDatabaseModule,
+    MatInputModule, MatFormFieldModule, MatButtonModule, MatIconModule, MatTabsModule
   ],
   providers: [LoginGuard],
   exports: [RouterModule, MatButtonModule, MatIconModule]
--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {AngularFireDatabase} from "angularfire2/database";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-charts',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class ChartsComponent implements OnInit {
+  @ViewChild("pieChart") xyEl: ElementRef;
 
-  constructor() { }
+  private xyListPath = "/xychart";
+
+  constructor(private dbService: AngularFireDatabase) { }
 
   ngOnInit() {
+    this.dbService.list(this.xyListPath).snapshotChanges()
+      .subscribe(result => {
+        const keys = result.map((s) => parseInt(s.key));
+        const values = result.map((s) => parseInt(s.payload.val()));
+        Plotly.purge(this.xyEl.nativeElement);
+        this.buildXYChart(keys, values);
+      });
   }
 
+  public async onAddXYItem(form: NgForm): Promise<void> {
+    if (form.valid){
+      const objectPath = this.xyListPath + "/" + form.value.x.toString();
+      await this.dbService.object<number>(objectPath).set(parseInt(form.value.y));
+    }
+  }
+
+
+  private buildXYChart(x: number[], y: number[]): void {
+    const element = this.xyEl.nativeElement;
+    const data = [{
+      x: x,
+      y: y
+    }];
+    const style = {
+      margin: {t: 0}
+    };
+
+    Plotly.plot(element, data, style);
+  }
 }
